054c9200227aaa445717d20954b993423a8c504b
David Agbesinyale <davidm7040@gmail.com>
2024-07-10 19:39:41 +0000

    Buy buy buy

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,4 @@
--- creates a trigger that decreases the quantity of an item after adding a new order
+--  lists all bands with Glam rock as their main style, ranked by their longevity
 SELECT band_name, (IFNULL(split, 2022) - formed) AS lifespan
     FROM metal_bands
     WHERE style LIKE '%Glam rock%'
