b03786b8f2410cdc09a30a2d2a7df98af1fb6863
David Agbesinyale <davidm7040@gmail.com>
2024-07-17 15:18:58 +0000

    updated

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -47,18 +47,15 @@ A decorator for recording the history of inputs and outputs of a specific functi
 
 def replay(method: Callable) -> None:
     """Function shows the call history of a specific function"""
-    key = method.__qualname__
-    inputs = key + ":inputs"
-    outputs = key + ":outputs"
-    server = method.__self__._redis
-    count = server.get(key).decode("utf-8")
-    print(f"{key} was called {count} times:")
-    input_list = server.lrange(inputs, 0, -1)
-    output_list = server.lrange(outputs, 0, -1)
-    zipped = list(zip(input_list, output_list))
-    for k, v in zipped:
-        attr, result = k.decode("utf-8"), k.decode("utf-8")
-        print(f"{key}(*{attr}) -> {result}")
+    name = method.__qualname__
+    cache = redis.Redis()
+    calls = cache.get(name).decode("utf-8")
+    print("{} was called {} times:".format(name, calls))
+    inputs = cache.lrange(name + ":inputs", 0, -1)
+    outputs = cache.lrange(name + ":outputs", 0, -1)
+    for i, o in zip(inputs, outputs):
+        print("{}(*{}) -> {}".format(name, i.decode('utf-8'),
+                                     o.decode('utf-8')))
 
 
 class Cache:
